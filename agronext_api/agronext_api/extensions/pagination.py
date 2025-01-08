53cbf659ae4ac458e572efa06ee9f0970234693e
Cristovam Silva <cristovam.lage@essor.com.br>
2025-01-08 18:30:54 -0300

    add paginate

--- agronext_api/agronext_api/extensions/pagination.py
+++ agronext_api/agronext_api/extensions/pagination.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi_pagination import paginate # noqa
 from fastapi_pagination import add_pagination
 from fastapi_pagination.utils import disable_installed_extensions_check
 
