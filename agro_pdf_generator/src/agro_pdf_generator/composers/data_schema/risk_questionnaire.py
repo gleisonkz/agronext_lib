a960f6caee4a5a7453351db3f03b9be42dbb135c
Joao Vitor Severo <joaovitor.severo@essor.com.br>
2026-03-11 16:17:26 -0300

    add answer to esg question removing the mocked value

--- agro_pdf_generator/src/agro_pdf_generator/composers/data_schema/risk_questionnaire.py
+++ agro_pdf_generator/src/agro_pdf_generator/composers/data_schema/risk_questionnaire.py
@@ -3,7 +3,9 @@ import agronext_procurement_repositories as repositories
 from ...schemas import RiskQuestionItem, RiskQuestionnaireData
 
 
-def build_quotation_risk_questionnaire(metadata: repositories.QuotationMetadata) -> RiskQuestionnaireData:
+def build_quotation_risk_questionnaire(
+    metadata: repositories.QuotationMetadata,
+) -> RiskQuestionnaireData:
     # Risk questionnaire
     another_insurance_question = RiskQuestionItem(
         question="Existe outro seguro contratado para a mesma área?",
@@ -44,7 +46,7 @@ def build_quotation_risk_questionnaire(metadata: repositories.QuotationMetadata)
 
     conservation_unit_question = RiskQuestionItem(
         question="Alguma parcela/talhão da área segurada está total ou parcialmente localizada em Unidade de Conservação (Federal/Estadual/Municipal), Áreas de Preservação Ambiental (exceto se houver aprovação oficial constante em plano de manejo), Área de Preservação Permanente, Reserva Legal, Área embargada por órgão ambiental (IBAMA, ICMBio ou órgão estadual/municipal), Terra Indígena, Território Quilombola ou qualquer área restrita, protegida ou embargada? (Estas informações são relevantes para a aceitação e precificação do seguro, conforme o art. 44 da Lei nº 15.040/2024. Omissões ou inexatidões podem implicar recusa da proposta, revisão de condições ou outras medidas previstas em lei e no contrato.)",
-        answer="Não"
+        answer="Sim" if metadata.esg_compliant else "Não",
     )
 
     risk_questionnaire_data = RiskQuestionnaireData(
