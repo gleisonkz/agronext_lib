04db48143eb4f6988d5f85872bc1ab72f1fa8e68
Pedro Vieira <pedrohenrique.vieira@essor.com.br>
2026-04-10 15:22:30 -0300

    suser code

--- agro_pdf_generator/src/agro_pdf_generator/composers/data_schema/broker.py
+++ agro_pdf_generator/src/agro_pdf_generator/composers/data_schema/broker.py
@@ -5,7 +5,7 @@ def build_quotation_broker(broker_details: dict) -> BrokerData:
     # Broker
     broker_data = BrokerData(
         name=broker_details.get("trade_name") or broker_details.get("preferred_name") or "Não informado",
-        susep="",
+        susep=broker_details.get("susep_code") or "Não informado",
         commission_pct="0.0%",
         phone=broker_details.get("phone") or "Não informado",
         emails=[broker_details.get("email") or "Não informado"],
@@ -19,7 +19,7 @@ def build_proposal_broker(broker_details: dict) -> BrokerData:
     # Broker
     broker_data = BrokerData(
         name=broker_details.get("trade_name") or broker_details.get("preferred_name") or "Não informado",
-        susep="",
+        susep= broker_details.get("susep_code") or "Não informado",
         commission_pct="0.0%",
         phone=broker_details.get("phone") or "Não informado",
         emails=[broker_details.get("email") or "Não informado"],
